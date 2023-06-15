1bf331cce240282cfb033d2deed841349b4f5bb6
Amanda Hilton <114438874+Amandapanda-hub@users.noreply.github.com>
2023-06-15 15:12:31 -0600

    adding mobile background switch from video to photo

--- src/pages/HomePage/components/Video/Video.js
+++ src/pages/HomePage/components/Video/Video.js
@@ -1,28 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import './Video.css'
 import sunSet from '../../../../assets/video/spa.mp4';
-
+import {mobileImage} from '../../../../assets/images/mobile-rose-water.png'
 
 export default function Video() {
-  return (
-    <>
+    const [width, setWidth] = useState(window.innerWidth);
 
-    <div className='hero'>
-        <video autoPlay loop muted id="video">
-            <source src={sunSet} type="video/mp4" />
-        </video>
-        <div className='content'>
-                <h1>Elité. Elegance.</h1>
-                <p>Luxury at Your Fingertips.</p>
+    useEffect(() => {
+      const handleWindowResize = () => setWidth(window.innerWidth);
+      window.addEventListener('resize', handleWindowResize);
+  
+      // Return a function from the effect that removes the event listener
+      return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
+    return (
+        <div className='hero'>
+          {width > 768 ? (
+            <video autoPlay loop muted id="video">
+              <source src={sunSet} type="video/mp4" />
+            </video>
+          ) : (
+            <img src={mobileImage} alt="Mobile view" id="image" />
+          )}
+          <div className='content'>
+            <h1>Elité. Elegance.</h1>
+            <p>Luxury at Your Fingertips.</p>
             <div>
-                <Link to='/' className='btn'>Reservations</Link>
-                <Link to='/contact' className='btn btn-light'>Contact</Link>
-                <Link to='/' className='btn'>Exploration</Link>
+              <Link to='/' className='btn'>Reservations</Link>
+              <Link to='/contact' className='btn btn-light'>Contact</Link>
+              <Link to='/' className='btn'>Exploration</Link>
             </div>
+          </div>
         </div>
-    </div>
-
-</>
-  )
-}
+      );
+    }
